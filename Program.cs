2917d600f622f8c2e0ddf199b9464be62d7bae4a
Ayon Alfaz <clone47@users.noreply.github.com>
2024-08-29 20:14:44 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+using System;
+using System.Diagnostics;
+
+public class SimpleQuery
+{
+    private const string MongoConnectionString = "mongodb+srv://ayonalfaz:!L0v3atlas@poccluster.mhbvh.mongodb.net/?retryWrites=true&w=majority&appName=POCCluster";
+
+    public static void Main(string[] args)
+    {
+        // allow automapping of the camelCase database fields to our MovieDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(MongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        var moviesCollection = mflixDatabase.GetCollection<MovieDocument>("movies");
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        // define and run pipeline
+        var results = moviesCollection.Aggregate()
+            .Search(Builders<MovieDocument>.Search.Text(movie => movie.Rated, "PG-13"))
+            .Project<MovieDocument>(Builders<MovieDocument>.Projection
+                .Include(movie => movie.Rated))
+            .Limit(10000)
+            .ToList();
+
+        // print results
+        foreach (var movie in results)
+        {
+            Console.WriteLine(movie.ToJson());
+        }
+
+        stopWatch.Stop();
+        // Get the elapsed time as a TimeSpan value.
+        TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class MovieDocument
+{
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public string? Rated { get; set; }
+}
